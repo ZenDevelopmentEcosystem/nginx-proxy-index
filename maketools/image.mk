18a488198b7fc91598ee81225d163c5131d139f2
Per Böhlin <per.bohlin@devconsoft.se>
2022-04-09 22:52:20 +0200

    Fix bug in json template

--- maketools/image.mk
+++ maketools/image.mk
@@ -1,11 +1,12 @@
 IMAGE := perbohlin/nginx-proxy-index
 TAG := test
+DOCKER_ARGS ?=
 
 INDEX_WEB.src ?= https://github.com/ZenDevelopmentEcosystem/index-web.git
 INDEX_WEB.type ?= git
 
 image:
-	$(Q)docker build . \
+	$(Q)docker build . $(DOCKER_ARGS)\
 		-t $(IMAGE):$(TAG) \
 		--build-arg "INDEX_WEB_SRC=$(INDEX_WEB.src)" \
 		--build-arg "INDEX_WEB_TYPE=$(INDEX_WEB.type)"
